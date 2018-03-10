ee44b568b8e57674aad1b43306fa5c8d0a1df233
Monir Cederstrom <monircederstrom@bootcamp.attlocal.net>
2018-03-09 21:34:42 -0600

    Fix bugs to allow to animate. Add CSS

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var singers = ["Destiny's Child", "Usher", "NSYNC", "Snoop Dog", "Tupac", "Lauryn Hill", "Backstreet Boys", "Boys II Men", "Mariah Carey", "Foo Fighters", "Brandy", "Matchbox Twenty", "No Doubt", "Nirvana", "Monica", "Hanson", "Smashing Pumpkins", "Pearl Jam", "Bell Biv DeVoe", "Dr. Dre"];
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + singers + "&api_key=LKu9IxoQTdCU5irRuQpXBXXT38RUIE70&limit=10&g=pg13";
 
+
+//Adding artist to array
 function renderButtons() {
 	$("#viewbuttons").empty();
 
@@ -13,10 +15,7 @@ function renderButtons() {
 	}
 }
 
-
-
-
-//link buttons to GIPHY API
+//adding buttons to document
 $("#add").on("click", function(event) {
 	event.preventDefault();
 	var music = $("#input").val().trim();
@@ -25,50 +24,58 @@ $("#add").on("click", function(event) {
 	renderButtons();
 });
 
-
-renderButtons();
-
-//link button click to giphys
+//function to call giphy api to buttons
 $(document).on("click", "button", function() {
-var singers = $(this).attr("data-name");
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + singers + "&api_key=LKu9IxoQTdCU5irRuQpXBXXT38RUIE70&limit=10&rating=pg13";
 
-$.ajax({
+	//link button click to giphys
+	var singers = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + singers + "&api_key=LKu9IxoQTdCU5irRuQpXBXXT38RUIE70&limit=10&rating=pg13";
+
+	$.ajax({
 	url: queryURL,
 	method: "GET"
-})
-.then(function(response) {
-	console.log(response);
-	var results = response.data;
-	//loop through each to append to div
-	for (var i = 0; i < results.length; i++) {
-		//create a new div to store giphys in
-		var singerDiv = $("<div>");
-		//create paragraph tag to display rating
-		var p = $("<p>").text("Rating: " + results[i].rating);
-		//create a img tag to display giph image
-		var singerImage = $("<img>");
-		//setting src attribute to image from results array in Still state
-		singerImage.attr("src", results[i].images.fixed_height_still.url);
-		//append p and img tag to singerDiv
-		singerImage.attr("data-state");
-	singerDiv.append(p);
-	singerDiv.append(singerImage);
-	//display singerDIV on HTML 
-	$("#giphs").prepend(singerDiv);
+	})
+	.then(function(response) {
+		console.log(response);
+		var results = response.data;
+		//loop through each to append to div
+		for (var i = 0; i < results.length; i++) {
+			//create a new div to store giphys in
+			var singerDiv = $("<div>");
+			//create paragraph tag to display rating
+			var p = $("<p>").text("Rating: " + results[i].rating);
+			//create a img tag to display giph image
+			var singerImage = $("<img>");
+
+			//append p and img tag to singerDiv
+			singerDiv.append(p);
+			singerDiv.append(singerImage);
+			//display singerDIV on HTML 
+			$("#giphs").prepend(singerDiv);
 
 
-	}
+				//add class of gif to image
+				singerImage.addClass("gif");
+				//set src attribute
+				singerImage.attr("src",results[i].images.fixed_height_small_still.url);
+				
+				singerImage.attr("data-still",results[i].images.fixed_height_small_still.url);
+				
+				singerImage.attr("data-animate",results[i].images.fixed_height_small.url); 
+
+		}
+
+		// on click function to animate or still
+			$(document).on("click", ".gif", function() {
+				var state = $(this).attr('data-state');
+				if (state == 'still') {
+					$(this).attr('src', $(this).data('animate'));
+					$(this).attr('data-state', 'animate');
+				}
+				else {
+				$(this).attr('src', $(this).data('still'));
+				$(this).attr('data-state', 'still');
+				}
+			});
+	});
 });
-});
-
-$(".music").on("click", function() {
-	var state = $(this) .attr("data-state");
-	if (state === "still") {
-		$(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
---
+++ assets/app.js
@@ -0,0 +1,105 @@
+var singers = ["Destiny's Child", "Usher", "NSYNC", "Snoop Dog", "Tupac", "Lauryn Hill", "Backstreet Boys", "Boys II Men", "Mariah Carey", "Foo Fighters", "Brandy", "Matchbox Twenty", "No Doubt", "Nirvana", "Monica", "Hanson", "Smashing Pumpkins", "Pearl Jam", "Bell Biv DeVoe", "Dr. Dre"];
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + singers + "&api_key=LKu9IxoQTdCU5irRuQpXBXXT38RUIE70&limit=10&g=pg13";
+
+
+//Adding artist to array
+function renderButtons() {
+	$("#viewbuttons").empty();
+
+	for (var i = 0; i < singers.length; i++) {
+		var a = $("<button>");
+		a.addClass("music");
+		a.attr("data-name", singers[i]);
+		a.text(singers[i]);
+		$("#viewbuttons").append(a);
+	}
+}
+
+//adding buttons to document
+$("#add").on("click", function(event) {
+	event.preventDefault();
+	var music = $("#input").val().trim();
+	singers.push(music);
+	$("")
+	renderButtons();
+});
+
+//function to call giphy api to buttons
+renderButtons();
+$(document).on("click", "button", function() {
+
+//link button click to giphys
+var singers = $(this).attr("data-name");
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + singers + "&api_key=LKu9IxoQTdCU5irRuQpXBXXT38RUIE70&limit=10&rating=pg13";
+
+$.ajax({
+	url: queryURL,
+	method: "GET"
+})
+.then(function(response) {
+	console.log(response);
+	var results = response.data;
+	//loop through each to append to div
+	for (var i = 0; i < results.length; i++) {
+		//create a new div to store giphys in
+		var singerDiv = $("<div>");
+		//create paragraph tag to display rating
+		var p = $("<p>").text("Rating: " + results[i].rating);
+		//create a img tag to display giph image
+	var singerImage = $("<img>");
+
+	//append p and img tag to singerDiv
+	singerDiv.append(p);
+	singerDiv.append(singerImage);
+	//display singerDIV on HTML 
+	$("#giphs").prepend(singerDiv);
+
+
+		//add class of gif to image
+		singerImage.addClass("gif");
+		//set src attribute
+		singerImage.attr("src",results[i].images.fixed_height_small_still.url);
+		
+		singerImage.attr("data-still",results[i].images.fixed_height_small_still.url);
+		
+		singerImage.attr("data-animate",results[i].images.fixed_height_small.url); 
+
+	}
+//});
+//});
+
+// on click function to animate or still
+$(document).on("click", ".gif", function() {
+	//var results = response.data;
+	//var src = $("img").attr("src");
+	//console.log("this is the button",$(this)
+		//[0].attributes["data-animate"].val();
+		var state = $(this).attr('data-state');
+	if (state == 'still') {
+	//$("img").removeAttr("src", results.data[i].images.fixed_height_still.url);
+		
+		//singerImage.attr("data-state","animate");
+		//var results = response.data;
+		//$(this).addClass("animate").attr("src", results[i].images.fixed_height.url);
+	
+		//$("img").removeClass("still");
+		//$("img").addClass("animate").attr("src", results[i].images.fixed_height.url);
+		$(this).attr('src', $(this).data('animate'));
+		$(this).attr('data-state', 'animate');
+	
+	}
+	else {
+	//console.log("already animated")
+	$(this).attr('src', $(this).data('still'));
+	$(this).attr('data-state', 'still');
+	//$(this).switchClass("animate", "still");
+		//$(this).attr("src", results[i].images.fixed_height.url);
+	}
+});
+});
+//        $(this).attr("data-state", "animate");
+      //} else {
+        //$(this).attr("src", results[i].images.fixed_height_still.url);
+        //$(this).attr("data-state", "still");
+      //}
+    });
